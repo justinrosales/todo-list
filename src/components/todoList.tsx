482a61078ca74c9c5fc1f59aa38ef101709dc4e4
justinrosales <rosalesjpn@gmail.com>
2025-02-20 15:27:09 -0800

    added CRUD commands and REACT components

---
+++ src/components/todoList.tsx
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import TodoTypes from "../todo";
+import TodoService from "../todoService";
+import { FaEdit, FaCheck } from "react-icons/fa";
+import { GiCancel } from "react-icons/gi";
+import { RiDeleteBin5Fill } from "react-icons/ri";
+
+const ComponentName = () => {
+  const [todos, setTodos] = useState<TodoTypes[]>(TodoService.getTodos());
+  const [editingTodoID, setEditedTodoID] = useState<number | null>(null);
+  const [editedTodoText, setEditedTodoText] = useState<string>("");
+
+  // handling edit actions
+  const handleEditStart = (id: number, text: string) => {
+    setEditedTodoID(id);
+    setEditedTodoText(text);
+  };
+
+  const handleEditCancel = () => {
+    setEditedTodoID(null);
+    setEditedTodoText("");
+  };
+
+  const handleEditSave = (id: number) => {
+    if (editedTodoText.trim() !== "") {
+      const updateTodo = TodoService.updateTodo({
+        id,
+        text: editedTodoText,
+        completed: false,
+      });
+
+      setTodos((prevTodos) =>
+        prevTodos.map((todo) => (todo.id === id ? updateTodo : todo))
+      );
+      setEditedTodoID(null);
+      setEditedTodoText("");
+    }
+  };
+
+  // function to delete todos
+  const handleDeleteTodo = (id: number) => {
+    TodoService.deleteTodo(id);
+    setTodos((prevTodo) => prevTodo.filter((todo) => todo.id !== id));
+  };
+
+  return (
+    <div className="todoContainer">
+      <div>{/* Your component code here */}</div>
+      {todos.map((todo) => (
+        <div className="items" key={todo.id}>
+          {editingTodoID == todo.id ? (
+            <div className="editedText">
+              <input
+                type="text"
+                value={editedTodoText}
+                onChange={(e) => setEditedTodoText(e.target.value)}
+                autoFocus={true}
+              />
+              <button onClick={() => handleEditSave(todo.id)}>
+                <FaCheck />
+              </button>
+
+              <button className="cancelbtn" onClick={() => handleEditCancel()}>
+                <GiCancel />
+              </button>
+            </div>
+          ) : (
+            <div className="editBtn">
+              <span>{todo.text}</span>
+              <button onClick={() => handleEditStart(todo.id, todo.text)}>
+                <FaEdit />
+              </button>
+            </div>
+          )}
+
+          <button onClick={() => handleDeleteTodo(todo.id)}>
+            <RiDeleteBin5Fill />
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default ComponentName;
---
+++ src/todoService.ts
@@ -0,0 +1,41 @@
+import TodoTypes from "./todo";
+
+const LOCAL_STORAGE_KEY = "todos";
+
+const TodoService = {
+  // READ
+  getTodos: (): TodoTypes[] => {
+    const todoStr = localStorage.getItem(LOCAL_STORAGE_KEY);
+    return todoStr ? JSON.parse(todoStr) : [];
+  },
+
+  //add todos // CREATE
+  addTodos: (text: string): TodoTypes[] => {
+    const todos = TodoService.getTodos();
+    const newTodo: TodoTypes = {
+      id: todos.length + 1,
+      text,
+      completed: false,
+    };
+    const updateTodos = [...todos, newTodo];
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(updateTodos));
+    return updateTodos; //used to be newtodo
+  },
+
+  //update todos // UPDATE
+  updateTodo: (todo: TodoTypes): TodoTypes[] => {
+    const todos = TodoService.getTodos();
+    const updateTodos = todos.map((t) => (t.id == todo.id ? todo : t));
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(updateTodos));
+    return todo; //used to be todo
+  },
+
+  //delete todos // DELETE
+  deleteTodo: (id: number): void => {
+    const todos = TodoService.getTodos();
+    const updateTodos = todos.filter((todos) => todos.id !== id);
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(updateTodos));
+  },
+};
+
+export default TodoService;
